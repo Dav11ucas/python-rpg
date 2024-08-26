727ef722f322bc01916d528fc81f07f2531d178d
David Pereira <lucas19pereira@hotmail.com>
2024-08-26 10:25:54 -0300

    criação de personagem com json

--- dungeonPrompt/src/main.py
+++ dungeonPrompt/src/main.py
@@ -1,76 +1,47 @@
-import random
+import json
 import os
 
-#global variables
-player_hp = 0
-player_damage = 0
-player_scape = 0
-player_dodge = 0
-npc_list = []
+with open('dungeonPrompt/src/classes.json', 'r') as classesfile:
+    classes_data = json.load(classesfile)
+with open('dungeonPrompt/src/classes.json', 'r') as enemyfile:
+    enemy_data = json.load(enemyfile)
+ 
+def character_creation():
+    character_name_length = 10
+    print('CHARACTER CREATION')
 
-def create_place():
-    #escolher uma lista de monstros, merdadores e qualquer outra possibilidade para as salas.
-    place_list = ['Swamp', 'Forest', 'Ruins', 'Cave']
-    place = random.choice(place_list)
-    return place
-
-def create_monster():
-    #cada monstro deve ter seus status de assim como o player, com o quanto de xp e loot que entrega.
-    #o loot deve ser randomico dentro de uma pequenda possibilidade de drop e não drop
-    if place == 'Swamp':
-        monster_list = [
-            {
-            'name': 'Swamp Slime'
-            }
-            ]
-    elif place == 'Forest':
-        monster_list = [
-            {
-            'name': 'Forest Slime'
-            }
-            ]
-    elif place == 'Ruins':
-        monster_list = [
-            {
-            'name': 'Ruins Slime'
-            }
-            ]
-    elif place == 'Cave':
-        monster_list = [
-            {
-            'name': 'Cave Slime'
-            }
-            ]
-
-#def create_merchant():
-#cada mercador deve ter seus produtos, e por enquanto é isso
-
-#def create_room():
-#cada sala deve ser padronizada, mas com suas próprias pequenas especificidades para cada lugar
-
-def create_player():
     while True:
-        os.system('cls')
-        print('''Atention, your initial status can only reach 10 with the sum of every items
-There will be DAMAGE, HEALTH, SCAPE, and DODGE
-              ''')
+        character_name = input(f'Type your character name (up to {character_name_length} characters): ')
+        if len(character_name) <= character_name_length:
+            break
+        else:
+            print(f'Please enter a name with less than {character_name_length} characters.')
+
+    print('These are all classes:')
+    class_names = [character_class['name'] for character_class in classes_data]
+    for i, name in enumerate(class_names):
+        print(f'{i+1}. {name}')
+
+    while True:
+        character_class_index = input('Select your character class (by number): ')
         try:
-            player_damage = int(input('select value to your damage: '))
-            player_hp = int(input('select value to your health: '))
-            player_scape = int(input('select value to your scapee: '))
-            player_dodge = int(input('select value to your dodge: '))
-            status_sum = player_damage + player_hp + player_scape + player_dodge
-            if status_sum == 10:
+            character_class_index = int(character_class_index) - 1
+            if 0 <= character_class_index < len(classes_data):
                 break
             else:
-                os.system('cls')
-                print("The sum of your stats must be equal to 10.")
-                accept = input()
+                print('Invalid class selection. Please choose a number from the list.')
         except ValueError:
-            os.system('cls')
-            print("Please enter only numbers.")
-os.system('clear')
+            print('Invalid input. Please enter a number.')
 
-#Game
+    chosen_class = classes_data[character_class_index]
+    print(f'\nCongratulations, {character_name}! You chose the {chosen_class["name"]} class.')
 
-create_player()
+def enemy_generator():
+    enemy_names = [enemys['name'] for enemys in enemy_data]
+    enemy_damage = [enemys['damage'] for enemys in enemy_data]
+    enemy_health = [enemys['health'] for enemys in enemy_data]
+    
+
+
+character_creation()
+enemy_generator()
